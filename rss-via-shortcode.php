ee9b3023c2faeb981b0962942a59a40d0269bda6
susantabeura <susantabeura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 04:40:19 +0000

    Initial release version
    git-svn-id: https://plugins.svn.wordpress.org/rss-via-shortcode-on-page-post/trunk@986041 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rss-via-shortcode.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: RSS Via Shortcode for Page & Post
+Version: 1.0.a
+Plugin URI: http://susantaslab.com/
+Description: Makes it easy to display an RSS feed on a page
+Author: Susanta K Beura
+Author URI: http://susantaslab.com/
+License: GPL v2
+Usages: [rssonpage rss="Your Feed URL" feeds="Number of Feeds" excerpt="summery true/false" target="_blank|_self|_top|_anyname"]
+*/
+
+function SLB_rss_sc( $atts ) {
+	extract(shortcode_atts(array(  
+	    "rss" 		=> '',  
+		"feeds"		=> '10',  
+		"excerpt" 	=> true,
+		"target"	=> '_blank'
+	), $atts));
+	require_once(ABSPATH.WPINC.'/rss.php');  
+	if ( $feed != "" && $rss = fetch_rss( $feed ) ) {
+		$content = '<ul>';
+		if ( $num !== -1 ) {
+			$rss->items = array_slice( $rss->items, 0, $num );
+		}
+		foreach ( (array) $rss->items as $item ) {
+			$content .= '<li>';
+			if ($target != '_self')
+				$content .= '<a href="'.esc_url( $item['link'] ).'" target="'.esc_attr($target).'">'. esc_html($item['title']) .'</a>';
+			else
+				$content .= '<a href="'.esc_url( $item['link'] ).'">'. esc_html($item['title']) .'</a>';
+			if ( $excerpt != false && $excerpt != "false") {
+				$content .= '<br/><span class="rss_excerpt">'. esc_html($item['description']) .'</span>';
+			}
+			$content .= '</li>';
+		}
+		$content .= '</ul>';
+	}
+	return $content;
+}
+
+add_shortcode( 'rssonpage', 'SLB_rss_sc' );
+
+?>
